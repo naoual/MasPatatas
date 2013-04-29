7fe02283d3174152b0a7b4a1b9119b8d78aef11a
mapacse <gatovolador96@gmail.com>
2013-04-29 10:03:38 +0200

    ServletPerfil

---
+++ GUC_1-war/src/java/servlets/ListaPerfilesServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import app.dao.PerfilgastoFacade;
+import app.entity.Perfilgasto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Juanito
+ */
+@WebServlet(name = "ListaPerfilesServlet", urlPatterns = {"/ListaPerfilesServlet"})
+public class ListaPerfilesServlet extends HttpServlet {
+    @PersistenceContext(unitName = "GUC_1-ejbPU" )
+    private EntityManager em;
+    
+    @EJB
+    private PerfilgastoFacade perfilgastofacade;
+    @EJB
+    private Perfilgasto perfil;
+
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       List<Perfilgasto> listaperfil = perfilgastofacade.findAll(); 
+       request.setAttribute("perfiles", listaperfil);
+       
+       RequestDispatcher rd;
+       rd = this.getServletContext().getRequestDispatcher("/GestionPerfiles.jsp");
+       
+       rd.forward(request, response);
+       
+       
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
